ccf17ef69fff2c80080b136aac1df980f54e3591
Joha Wilson <johawilsonatu@icloud.com>
2024-03-06 01:39:32 +0000

    Created a styled Header component to handle styles

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -7,7 +7,7 @@ const ControlContainer = styled.div`
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`
+`;
 
 const Label = styled.label`
   display: block;
@@ -16,20 +16,20 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: ${({props}) => (props.invalid ? '#f87171' : ' #6b7280')}
-`
+  color: ${({ props }) => (props.invalid ? '#f87171' : ' #6b7280')}
+`;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: ${({invalid}) => (invalid ? '#fed2d2' : '#d1d5db')} 
-  color: ${({invalid}) => (invalid ? '#ef4444' : ' #374151')}
-  border: 1px solid ${({invalid}) => (invalid ? '#f73f3f' : 'transparent')}
+  background-color: ${({ invalid }) => (invalid ? '#fed2d2' : '#d1d5db')} 
+  color: ${({ invalid }) => (invalid ? '#ef4444' : ' #374151')}
+  border: 1px solid ${({ invalid }) => (invalid ? '#f73f3f' : 'transparent')}
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 
-`
+`;
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
