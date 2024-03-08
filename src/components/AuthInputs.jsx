a15a36b17e8a92115c0edbc357fc162981ca077b
Joha-will <johawilsonatu@icloud.com>
2024-03-08 22:13:50 +0000

    Added tailwind css style to Authinput.jsx container and buttons

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,15 +1,7 @@
 import { useState } from 'react';
-import { styled } from 'styled-components'
 import Button from './Button.jsx';
 import Input from './Input.jsx';
 
-const ControlContainer = styled.div`
-
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -17,7 +9,7 @@ export default function AuthInputs() {
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -32,10 +24,10 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
-        <p className='paragraph'>
-          <Input label="Email" invalid={emailNotValid} type="email" onChange={(event) => handleInputChange('email', event.target.value)}/>
+    <div id="auth-inputs" className="mx-auto w-full max-w-sm p-8 rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col mb-6 gap-2">
+        <p className="paragraph">
+          <Input label="Email" invalid={emailNotValid} type="email" onChange={(event) => handleInputChange("email", event.target.value)}/>
         </p>
         <p>
           <Input
@@ -43,13 +35,13 @@ export default function AuthInputs() {
             type="password"
             invalid={passwordNotValid}
             onChange={(event) =>
-              handleInputChange('password', event.target.value)
+              handleInputChange("password", event.target.value)
             }
           />
         </p>
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
